aeb9981ecd74bf0f7c820e71af624938e7aee77a
Sri Kavya Pagadala <srikavya.pagadala@gmail.com>
2026-05-08 15:25:30 +0530

    fixedd allow texts

--- backend/student_management/settings.py
+++ backend/student_management/settings.py
@@ -10,9 +10,11 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get(
-    'ALLOWED_HOSTS', 'localhost,127.0.0.1'
-).split(',')
+ALLOWED_HOSTS = [
+    "student-management-system-4tfa.onrender.com",
+    "127.0.0.1",
+    "localhost"
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
